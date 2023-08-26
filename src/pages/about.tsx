014d8d4f0568f1a2f659de1eebb37914633ee39e
gylychmedov <howesjen.01@gmail.com>
2023-08-26 00:55:05 -0700

    Translate completed, about image changed

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,14 +7,14 @@ import { AnimatePresence, motion } from "framer-motion";
 import useTranslation from "next-translate/useTranslation";
 
 const AboutPage = () => {
-  const { t } = useTranslation();
+  const { t } = useTranslation("about");
   const [collapsed, setCollapsed] = useState(false);
   return (
     <Layout title="Contact" className="bg-white">
       <main className="grid grid-cols-12 gap-3 md:pt-5 md:pb-10 lg:gap-10 contain">
         <section className="col-span-12 md:col-span-6">
           <img
-            src="https://img.freepik.com/free-photo/mature-lawyer-pointing-signature-place-contract-document-with-pen_23-2147898521.jpg?w=740&t=st=1687043430~exp=1687044030~hmac=1177df0c261944dadba8de6680e79a73498560cb4a4f904986c125afec6bde74"
+            src="/images/pages/about.png"
             alt=""
             className="w-full h-96 object-cover rounded-xl mt-10"
           />
@@ -32,21 +32,8 @@ const AboutPage = () => {
             onClick={() => setCollapsed(!collapsed)}
             className="border-l-2 pl-5 border-pink-650 text-teal-850 cursor-pointer"
           >
-            The "GM-Dream Team" is the pioneering grant agency in Central Asia,
-            dedicated to empowering the civil society sector and driving
-            positive change within local communities. The agency's mission is to
-            train young leaders in the field of grant management, equipping them
-            with the knowledge and skills necessary to meet the requirements of
-            grantor organizations throughout the entire grant lifecycle.
-            Unfortunately, many local youths are unaware of the vast career
-            opportunities in grant management due to a lack of information.
-            Professions such as grant writer, grant project manager, monitoring
-            and evaluation (M&E) expert, financial consultant, and head of a
-            civil society organization hold immense appeal for professionals
-            seeking flexible working hours, remote work options, the ability to
-            select clients and projects based on their interests, and the
-            opportunity to bring their own ideas to life while contributing to
-            local community development.
+            {t("about_1")}
+
             {!collapsed && (
               <span className="ml-4 text-blue-500">
                 {t("common:read_more")}
@@ -63,46 +50,15 @@ const AboutPage = () => {
                 className="cursor-pointer"
               >
                 <article className="border-l-2 pl-5 border-pink-650 text-teal-850 mt-3">
-                  The "GM-Dream Team" aims to promote these professions among
-                  young people and provide comprehensive training in each role.
-                  These young grant experts will possess the agility to respond
-                  swiftly to emerging opportunities. By doing so, this
-                  initiative will foster a new generation of forward-thinking
-                  civil society leaders who can identify funding opportunities,
-                  align project ideas with the mission goals of grantor
-                  organizations, and actively contribute to addressing the needs
-                  of their local communities. These young grant experts will
-                  have the choice to become independent freelance professionals,
-                  join existing civil society organizations, establish their own
-                  organizations, or even become trainers in the field of grant
-                  management.
+                  {t("about_2")}
                 </article>
 
                 <article className="border-l-2 pl-5 border-pink-650 text-teal-850 mt-3">
-                  Guncha Komekova, the founder of the "GM-Dream Team" grant
-                  agency, brings fifteen years of experience coordinating grant
-                  programs at the donor's organization. Guncha imparts her
-                  knowledge to the next generation of grant experts, she trained
-                  her team extensively in grant project management throughout
-                  the entire grant lifecycle. Her focus includes M&E of grant
-                  projects, audience analysis, composing comprehensive grant
-                  packages, and overseeing the grant management process in
-                  compliance with grant policies. Guncha recognizes the agency's
-                  primary role in attracting youth to the field by promoting
-                  grant management professions. These experts will play a
-                  pivotal role in providing civil society actors with the
-                  necessary resources to strengthen their work and address the
-                  priorities of their communities.
+                  {t("about_3")}
                 </article>
 
                 <article className="border-l-2 pl-5 border-pink-650 text-teal-850 mt-3">
-                  In addition to its training and consultation services, the
-                  "GM-Dream Team" grant agency also provides coaching on turnkey
-                  grant management to organizations seeking comprehensive
-                  support throughout the entire grant process. These turnkey
-                  services offer a seamless and efficient approach, relieving
-                  organizations of the burden of dealing with bureaucracies,
-                  allowing them to focus on their core mission and objectives.
+                  {t("about_4")}
                 </article>
               </motion.div>
             )}
