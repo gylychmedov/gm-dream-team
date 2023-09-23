386643574282339635e2137f6b7cae39c36ae29a
gylychmedov <howesjen.01@gmail.com>
2023-09-23 00:18:37 -0700

    Favicon added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import "@/styles/tailwind.css";
 import "swiper/css";
 import type { AppProps } from "next/app";
 import Watcher from "@/components/Elements/Watcher";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/logo.png" />
+      </Head>
       <Watcher />
       <Component {...pageProps} />
     </>
